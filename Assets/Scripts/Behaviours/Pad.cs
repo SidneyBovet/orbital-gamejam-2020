d574ba3d402cc6f3196eb6b4d093089d657915ea
Thomas 'Raste' Rouvinez <trouvinez@logitech.com>
2020-02-22 17:48:48 +0100

    Create polarity inverters

--- Assets/Scripts/Behaviours/Pad.cs
+++ Assets/Scripts/Behaviours/Pad.cs
@@ -4,18 +4,40 @@ using UnityEngine;
 
 public class Pad : MonoBehaviour
 {
+    public enum Polarity {
+        POSITIVE,
+        NEGATIVE
+    }
+
+    public Polarity polarity;
     public Material mMatPositiveCharge;
     public Material mMatNegativeCharge;
 
     // Start is called before the first frame update
     void Start()
     {
+        Renderer renderer = GetComponent<Renderer>();
 
+        // Verify charge, apply correct material
+        if (polarity == Polarity.POSITIVE)
+        {
+            renderer.material = mMatPositiveCharge;
+        }
+        else
+        {
+            renderer.material = mMatNegativeCharge;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
-
+        if (polarity == Polarity.POSITIVE)
+        {
+            GameManager.Instance.currentCharge = 1;
+        }
+        else
+        {
+            GameManager.Instance.currentCharge = -1;
+        }
     }
 }
