79298f0361a0dfa33af6c8b36812eab43155ac0b
Thomas 'Raste' Rouvinez <trouvinez@logitech.com>
2020-02-22 16:53:45 +0100

    Add game manager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    static GameManager instance = null;
+    static readonly object padlock = new object();
+
+    GameManager() { }  // Block instantiation
+
+    // Prevent deleting this object between scene changes
+    void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    public static GameManager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                lock (padlock)
+                {
+                    if (instance == null)
+                    {
+                        instance = new GameManager();
+                    }
+                }
+            }
+            return instance;
+        }
+    }
+
+    // Shared gamestate
+    public int currentLevel = 0;
+    public int score = 0;
+    public bool isInMenu = true;
+
+    public void reset()
+    {
+        currentLevel = 0;
+        score = 0;
+        isInMenu = true;
+    }
+}
