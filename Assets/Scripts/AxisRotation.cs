9a64f9333c3807be9ff67fbb2552ca053a45d539
Sidney Bovet <sbovet@logitech.com>
2020-02-22 17:18:48 +0100

    Rework movement

--- Assets/Scripts/AxisRotation.cs
+++ Assets/Scripts/AxisRotation.cs
@@ -4,20 +4,23 @@ using UnityEngine;
 
 public class AxisRotation : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
+    [Range(0f, 90f)]
+    public float maxRotation = 45;
 
-    }
+    [Range(0f, 0.1f)]
+    public float rotationSpeed = 0.05f;
+
+    private Quaternion m_targetRotation = Quaternion.identity;
 
     // Update is called once per frame
     void Update()
     {
-        float x = Input.GetAxis("Vertical");
-        float z = -Input.GetAxis("Horizontal");
+        float x = Input.GetAxis("Vertical") * maxRotation;
+        float z = -Input.GetAxis("Horizontal") * maxRotation;
 
         Debug.Log("x: " + x + ", z: " + z);
 
-        transform.Rotate(x, 0, z);
+        m_targetRotation = Quaternion.Euler(x, 0, z);
+        transform.rotation = Quaternion.Slerp(transform.rotation, m_targetRotation, rotationSpeed);
     }
 }
