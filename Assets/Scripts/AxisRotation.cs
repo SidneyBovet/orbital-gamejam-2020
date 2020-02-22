769014a8288cce155a12ce8da419cdfa3ca63827
Sidney Bovet <sbovet@logitech.com>
2020-02-22 16:32:23 +0100

    First mechanic

---
+++ Assets/Scripts/AxisRotation.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AxisRotation : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Vertical");
+        float z = -Input.GetAxis("Horizontal");
+
+        Debug.Log("x: " + x + ", z: " + z);
+
+        transform.Rotate(x, 0, z);
+    }
+}
