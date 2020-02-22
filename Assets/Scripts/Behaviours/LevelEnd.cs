e954ec2e2f8f32c5233f82e6aca9d5d03d1dfb78
Thomas 'Raste' Rouvinez <trouvinez@logitech.com>
2020-02-22 17:10:38 +0100

    Add prefab for end of level

---
+++ Assets/Scripts/Behaviours/LevelEnd.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelEnd : MonoBehaviour
+{
+    public GameObject chargeHolder;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // Check that the ball has entered
+        if (other.gameObject.name == "Ball") {
+            // Game is won, move to the next level
+            Debug.Log("Level is won");
+        }
+    }
+}
