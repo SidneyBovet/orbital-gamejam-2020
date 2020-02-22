31976127ab878d43a9deefc01d623f4da864d935
Sidney Bovet <sbovet@logitech.com>
2020-02-22 17:57:46 +0100

    Add level transition, and level 1

--- Assets/Scripts/Behaviours/LevelEnd.cs
+++ Assets/Scripts/Behaviours/LevelEnd.cs
@@ -1,10 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LevelEnd : MonoBehaviour
 {
     public GameObject chargeHolder;
+
+    [Range(0,5)]
+    public float nextLevelLoadDelay = 1f;
+
+    private bool m_nextSceneLoading = false;
 
     private void OnTriggerEnter(Collider other)
     {
@@ -12,8 +18,17 @@ public class LevelEnd : MonoBehaviour
         if (other.gameObject.name == "Ball") {
             // Game is won, move to the next level
             Debug.Log("Level is won");
-
-            GameManager.Instance.currentLevel++;
+            GameManager.Instance.currentLevel += 1;
+            StartCoroutine(LoadNextLevel(GameManager.Instance.currentLevel));
         }
+    }
+
+    private IEnumerator LoadNextLevel(int levelNumber)
+    {
+        Debug.Log("Loading level " + levelNumber + " in " + nextLevelLoadDelay + "s");
+
+        yield return new WaitForSeconds(nextLevelLoadDelay);
+
+        SceneManager.LoadScene("Level-" + levelNumber);
     }
 }
