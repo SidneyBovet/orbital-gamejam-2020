3b9ae85525a7acc6d118b1ce2102e729a496b1f3
Thomas 'Raste' Rouvinez <trouvinez@logitech.com>
2020-02-23 11:05:50 +0100

    Add RT detection for paint

---
+++ Assets/Scripts/Behaviours/PaintRenderer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaintRenderer : MonoBehaviour
+{
+    LineRenderer paint;
+    public float trigger;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        paint = GetComponent<LineRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        trigger = Input.GetAxis("Fire1") * 40.0f;
+    }
+}
