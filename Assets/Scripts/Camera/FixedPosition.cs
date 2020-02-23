22c910a6c00fb3166b55b40b4bedefcf9e7ccd44
Thomas 'Raste' Rouvinez <trouvinez@logitech.com>
2020-02-23 08:19:19 +0100

    Add charge indicator on ball

--- Assets/Scripts/Camera/FixedPosition.cs
+++ Assets/Scripts/Camera/FixedPosition.cs
@@ -5,18 +5,27 @@ using UnityEngine;
 public class FixedPosition : MonoBehaviour
 {
     public GameObject target;
-    public float cameraRotation;
+    public GameObject chargeIndicator;
+
+    float cameraRotation;
+    Vector3 indicatorOffset;
     Quaternion rotation;
+    TextMesh indicator;
 
     void Start()
     {
         rotation = target.transform.rotation;
+        indicatorOffset = chargeIndicator.transform.position - target.transform.position;
+        indicator = chargeIndicator.GetComponent<TextMesh>();
     }
 
     // Update is called once per frame
     void Update()
     {
         gameObject.transform.position = target.transform.position;
+
+        chargeIndicator.transform.position = target.transform.position + indicatorOffset;
+        indicator.text = "" + GameManager.Instance.currentCharge;
 
         // Camera rotation
         float cameraRotation = -Input.GetAxis("Mouse X") * 80;
